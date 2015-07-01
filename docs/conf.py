02ec96f97eb883a91e59a5d2f1a09b595c612240
Luc Saffre <luc.saffre@gmail.com>
2015-07-01 06:15:03 +0300

    http://luc.lino-framework.org/blog/2015/0701.html

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ extensions += ['lino.sphinxcontrib.logo']
 extensions += ['lino.sphinxcontrib.actordoc']
 extensions += ['sphinxcontrib.taglist']
 extensions += ['sphinxcontrib.youtube']
+extensions += ['sphinx.ext.inheritance_diagram']
 
 extensions += ['atelier.sphinxconf.sigal_image']
 
@@ -316,3 +317,8 @@ html_sidebars = {
 # html_theme_options = dict(collapsiblesidebar=True, externalrefs=True)
 
 blogref_format = "http://luc.lino-framework.org/blog/%Y/%m%d.html"
+
+
+inheritance_graph_attrs = dict(rankdir="TB")
+inheritance_graph_attrs.update(size='"12.0, 16.0"')
+inheritance_graph_attrs.update(fontsize=14, ratio='compress')
