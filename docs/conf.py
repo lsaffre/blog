0e0f0f55d337f4e0ce94c98fa889195958d78d2c
Luc Saffre <luc.saffre@gmail.com>
2015-09-27 17:35:39 +0300

    http://luc.lino-framework.org/blog/2015/0927.html

--- docs/conf.py
+++ docs/conf.py
@@ -323,3 +323,9 @@ blogref_format = "http://luc.lino-framework.org/blog/%Y/%m%d.html"
 inheritance_graph_attrs = dict(rankdir="TB")
 inheritance_graph_attrs.update(size='"12.0, 16.0"')
 inheritance_graph_attrs.update(fontsize=14, ratio='compress')
+
+my_font_family = "Swiss, Helvetica, 'Liberation Sans'"
+html_theme_options = {
+    "font_family": my_font_family,
+    "head_font_family": my_font_family,
+}
