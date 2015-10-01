5959b630634bf6a43fb792f1e3e946a889999f05
Luc Saffre <luc.saffre@gmail.com>
2015-10-01 17:09:25 +0300

    http://luc.lino-framework.org/blog/2015/1001.html

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,8 @@ interproject.configure(globals())
 extensions += ['atelier.sphinxconf.blog']
 extensions += ['atelier.sphinxconf.complex_tables']
 extensions += ['lino.sphinxcontrib.logo']
-extensions += ['lino.sphinxcontrib.actordoc']
-#extensions += ['sphinxcontrib.taglist']
+# extensions += ['lino.sphinxcontrib.actordoc']
+# extensions += ['sphinxcontrib.taglist']
 extensions += ['sphinxcontrib.youtube']
 extensions += ['sphinx.ext.inheritance_diagram']
 
