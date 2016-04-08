1e0d779976411477cf8a3f18d4676172e096e98a
Luc Saffre <luc.saffre@gmail.com>
2016-04-08 03:38:52 +0300

    http://luc.lino-framework.org/blog/2016/0408.html

---
+++ docs/blog/2016/0406.py
@@ -0,0 +1,20 @@
+from lino import startup
+startup('lino_welfare.projects.eupen.settings.demo')
+
+from atelier.rstgen import table
+from lino.api.shell import *
+from lino_welfare.modlib.welfare.roles import *
+
+roles = [OfficeOperator, OfficeUser, SepaUser, DebtsUser, LedgerStaff,
+         Supervisor, SiteAdmin]
+rows = []
+headers = ["Profile"]
+for r in roles:
+    headers.append(r.__name__)
+for p in users.UserProfiles.objects():
+    row = [p.text]
+    for r in roles:
+        row.append("X" if p.has_required_roles([r]) else "")
+    rows.append(row)
+
+print(table(headers, rows))
