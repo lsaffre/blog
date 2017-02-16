9ed88587571f5e56fab2821a4af2df4088146d29
Luc Saffre <luc.saffre@gmail.com>
2017-02-16 22:12:04 +0200

    http://luc.lino-framework.org/blog/2017/0216.html

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from lino.invlib.ns import ns
 ns.setup_from_tasks(globals())
 
 cfg = dict()
-cfg.update(tolerate_sphinx_warnings=True)
+cfg.update(tolerate_sphinx_warnings=False)
 cfg.update(blog_root='/home/luc/work/blog/')
 cfg.update(languages=['en'])
 cfg.update(revision_control_system='git')
