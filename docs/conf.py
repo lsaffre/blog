84758761719128f840ef59332e876c31d260382a
Luc Saffre <luc.saffre@gmail.com>
2018-01-13 07:59:38 +0200

    http://luc.lino-framework.org/blog/2018/0113.html

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = "The Lino blog"
-copyright = '2001-2017 Luc Saffre'
+copyright = '2001-2018 Luc Saffre'
 
 
 # The version info for the project you're documenting, acts as replacement for
