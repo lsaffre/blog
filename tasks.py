7925b442f8822ce557cca4aa5b34f37ceb634d74
Luc Saffre <luc.saffre@gmail.com>
2019-03-06 23:34:02 +0200

    http://luc.lino-framework.org/blog/2019/0306.html

--- tasks.py
+++ tasks.py
@@ -1,7 +1,7 @@
 from atelier.invlib import setup_from_tasks
 
 cfg = dict()
-cfg.update(tolerate_sphinx_warnings=True)
+# cfg.update(tolerate_sphinx_warnings=True)
 cfg.update(blog_root='/home/luc/work/blog/')
 cfg.update(languages=['en'])
 cfg.update(doc_trees=['docs'])
