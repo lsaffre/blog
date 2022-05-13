a087b3446822fa3c8e6fc00f0feec246919f6d0e
Luc Saffre <luc.saffre@gmail.com>
2022-05-13 07:12:41 +0300

    commit recent changes

---
+++ docs/blog/2022/0427/second.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import cv2
+import numpy as np
+
+filename1 = str(Path('~/Videos/20220427/video-1650779715.mp4').expanduser())
+filename2 = str(Path('~/Videos/20220427/2022-04-25-120810.webm').expanduser())
+
+
+cap1 = cv2.VideoCapture(filename1, 0)
+cap2 = cv2.VideoCapture(filename2,0)
+
+# The video 1 set the video 1 as the default size and fps
+fps1 = cap1.get(cv2.CAP_PROP_FPS)
+h1 = cap1.get(cv2.CAP_PROP_FRAME_HEIGHT)
+w1 = cap1.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+fps2 = cap2.get(cv2.CAP_PROP_FPS)
+h2 = cap2.get(cv2.CAP_PROP_FRAME_HEIGHT)
+w2 = cap2.get(cv2.CAP_PROP_FRAME_WIDTH)
+
+size = (int(w1+w2), int(h1+h2))
+
+
+print("{}: {}x{} ({} fps)".format(filename1, h1, w1, fps1))
+print("{}: {}x{} ({} fps)".format(filename2, h2, w2, fps2))
+
+fps = fps1
+
+fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+out = cv2.VideoWriter("output.avi", fourcc, fps, size)
+
+while(cap1.isOpened()):
+
+    ret, frame1 = cap1.read()
+    if not ret:
+        print("Read failure in {}".format(filename1))
+        break
+    ret, frame2 = cap2.read()
+    if not ret:
+        print("Read failure in {}".format(filename2))
+        break
+    both = np.concatenate((frame1, frame2), axis=1)
+
+    cv2.imshow('Frame', both)
+    out.write(both)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap1.release()
+out.release()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- docs/shared/include/part_of_lino.rst
+++ docs/shared/include/part_of_lino.rst
@@ -1,8 +1,8 @@
-This website is part of the Lino documentation. 
-You might also want to check the other websites:
-`Lino framework <https://www.lino-framework.org>`__,
+This website is part of the Lino documentation, which
+includes the following websites:
+`Main website <https://www.lino-framework.org>`__,
 `Community Guide <https://community.lino-framework.org>`__,
 `User Guide <https://using.lino-framework.org>`__,
 `Hosting Guide <https://hosting.lino-framework.org>`__,
-`Developer Guide <https://dev.lino-framework.org>`__ or
+`Developer Guide <https://dev.lino-framework.org>`__ and
 `Luc's developer blog <https://luc.lino-framework.org>`__.
