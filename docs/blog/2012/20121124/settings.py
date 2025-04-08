4377b5dfc4fdd5facef0307d87cfab46fa34505d
Luc Saffre <luc.saffre@gmail.com>
2025-04-08 09:43:46 +0300

    Rename mixins SalesDocument and ProductDocItem to TradingVoucher and TradingVoucherItem, respectively.

---
+++ docs/blog/2012/20121124/settings.py
@@ -0,0 +1,29 @@
+from lino.projects.std.settings import *
+
+# configure_plugin('countries', country_code='BE')
+
+
+class Site(Site):
+
+    verbose_name = "20121124"
+
+    # demo_fixtures = ["few_countries", "few_cities", "demo"]
+
+    def get_installed_plugins(self):
+        yield super(Site, self).get_installed_plugins()
+        yield 'lino_book.projects.20121124'
+
+
+SITE = Site(globals())
+DEBUG = True
+
+# INSTALLED_APPS = ['lino_book.projects.20121124']
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': ':memory:'
+#     }
+# }
+
+#SECRET_KEY = "123"
